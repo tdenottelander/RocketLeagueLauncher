5ce21db58ca22b5bb42956e0c75d25fdf69856cd
Tom den Ottelander <tdenottelander@gmail.com>
2018-06-12 23:02:46 +0200

    Add image to launcher

--- src/main/java/launcher/GuiFrame.java
+++ src/main/java/launcher/GuiFrame.java
@@ -1,8 +1,12 @@
 package main.java.launcher;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class GuiFrame extends JFrame
@@ -11,16 +15,19 @@ public class GuiFrame extends JFrame
     final JFrame guiFrame;
     final JPanel mainPane;
     final JPanel textPanel;
+    final JLabel imageLabel;
     final JPanel buttonPanel;
 
     public GuiFrame()
     {
         this.guiFrame = new JFrame();
         this.mainPane = new JPanel();
+        this.imageLabel = new JLabel();
         this.textPanel = new JPanel();
         this.buttonPanel = new JPanel();
         setupGuiFrame();
         setupMainPane();
+        createImage();
         createTextField();
         createButtons();
 
@@ -31,7 +38,7 @@ public class GuiFrame extends JFrame
         //make sure the program exits when the frame closes
         guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         guiFrame.setTitle("RocketLeagueLauncher");
-        guiFrame.setSize(300,250);
+        guiFrame.setSize(300,350);
         //This will center the JFrame in the middle of the screen
         guiFrame.setLocationRelativeTo(null);
     }
@@ -39,6 +46,19 @@ public class GuiFrame extends JFrame
     private void setupMainPane(){
         mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.PAGE_AXIS));
         guiFrame.add(mainPane);
+    }
+
+    private void createImage(){
+        BufferedImage img = null;
+        try
+        {
+            img = ImageIO.read(new File("src/main/resources/images/rl_icon_resized.png"));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        JLabel picLabel = new JLabel(new ImageIcon(img));
+        textPanel.add(picLabel);
     }
 
     private void createTextField(){
