b60381b0761de010a11ce27903c2fa69c33a57dc
Tom den Ottelander <tdenottelander@gmail.com>
2018-06-19 17:58:42 +0200

    Add reading from a settingsfile

--- src/main/java/launcher/App.java
+++ src/main/java/launcher/App.java
@@ -4,18 +4,21 @@ import main.java.launcher.configmodifier.RLConfigModifier;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class App
 {
     static RLConfigModifier configModifier;
+    static final String settingsFilePath = "src/main/resources/settings.txt";
+    static String RLSettingsFileLocation;
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        readSettings();
 
         GuiFrame guiFrame = new GuiFrame();
 
-        String filePath = "src/test/resources/TASystemSettings.ini";
+        String filePath = RLSettingsFileLocation + "TASystemSettings.ini";
         configModifier = new RLConfigModifier(filePath);
         configModifier.readFile();
         configModifier.setOutputFilePath(filePath);
@@ -41,5 +44,26 @@ public class App
                 System.out.println("Button Wide Click");
             }
         });
+    }
+
+    /**
+     * Reading the settings from the settings.txt file
+     */
+    private static void readSettings(){
+        File file = new File(settingsFilePath);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = bufferedReader.readLine();
+            if(line.contains("location")){
+                String[] lines = line.split("=");
+                RLSettingsFileLocation = lines[1];
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
